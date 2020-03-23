df2c9bd2d91eac3759313f4580a6124bfc047823
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-03-23 15:25:17 -0500

    update views for Auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Place
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class PlaceCreate(CreateView):
+class PlaceCreate(LoginRequiredMixin,CreateView):
     model = Place
     fields = '__all__'
 
-class PlaceUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class PlaceUpdate(LoginRequiredMixin, UpdateView):
     model = Place
     fields = ['country', 'state', 'city', 'description']
 
-class PlaceDelete(DeleteView):
+class PlaceDelete(LoginRequiredMixin, DeleteView):
     model = Place
     success_url = '/places/'
 
@@ -21,11 +29,26 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def places_index(request):
-    places = Place.objects.all()
+    places = Place.objects.filter(user=request.user)
     return render(request, 'places/index.html', {'places': places})
 
+@login_required
 def places_detail(request, place_id):
     place = Place.objects.get(id=place_id)
     return render(request, 'places/detail.html', {'place': place})
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
