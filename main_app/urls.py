3e6d2cb2cc077b28d31a443c53f9d6d1006e2725
90dandan <1brokendrumhead@gmail.com>
2020-03-23 11:22:28 -0500

    index url path

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('places/', views.places_index, name='index'),
 ]
