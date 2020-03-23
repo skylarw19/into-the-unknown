acc1cbc4f2c6e7e62eb7e86a2f1f0c5cd7489ccd
90dandan <1brokendrumhead@gmail.com>
2020-03-23 14:23:20 -0500

    update and delete url

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('places/', views.places_index, name='index'),
     path('places/<int:place_id>/', views.places_detail, name='places_detail'),
     path('places/create/', views.PlaceCreate.as_view(), name='places_create'),
+    path('places/<int:pk>/update/', views.PlaceUpdate.as_view(), name='places_update'),
+    path('places/<int:pk>/delete/', views.PlaceDelete.as_view(), name='places_delete'),
 ]
